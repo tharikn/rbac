b77e930109d7293a2be7c9c35e6f51dcaa8db2b3
tharikn <110445663+tharikn@users.noreply.github.com>
2024-01-02 11:46:49 +0530

    v45

--- fesm2015/pics-module-microstrategy.js
+++ fesm2015/pics-module-microstrategy.js
@@ -266,9 +266,12 @@ class MicrostrategyService {
         });
     }
     getDossier(projectId, dossierId, pageNo) {
-        projectId = 'DFC8E31E4EF39DE1113D1EACD5B30C9C';
+        // projectId = 'DFC8E31E4EF39DE1113D1EACD5B30C9C';
+        // dossierId = 'EC9258B346017C2D961A2EA38010599D';
+        // pageNo = 'K53--K46/edit';
+        projectId = '536D07A04970382CA52070B391E01998';
         dossierId = 'EC9258B346017C2D961A2EA38010599D';
-        pageNo = 'K53--K46/edit';
+        pageNo = 'W111--K46/edit';
         const permissions = this.permissionStore.state;
         const projectUrl = `${this.environment.mstrURL}/app/${projectId}`;
         const dossierUrl = `${projectUrl}/${dossierId}/${pageNo}`;
