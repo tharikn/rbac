fb267f576ca256052d2bba58b69ff3ab0ae4e758
Tharikniyas <tharikniyas.m@particleblack.com>
2024-05-14 20:59:04 +0530

    v-updated

--- lib/pics-profile/@core/service/attachments.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { HttpService } from './http.service';
-import * as i0 from "@angular/core";
-export declare class AttachmentsService {
-    private http;
-    constructor(http: HttpService);
-    uploadKey(objparams: any): import("rxjs").Observable<Object>;
-    static ɵfac: i0.ɵɵFactoryDeclaration<AttachmentsService, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<AttachmentsService>;
-}
